7d4b97c631779fb91ab2dace97d6dd6981e85dc6
johnnybegood1318 <johnnybegood1414@gmail.com>
2017-10-15 20:02:05 -0400

    update support from ide error

--- support/evaluator/IllegalPostFixExpressionException.java
+++ support/evaluator/IllegalPostFixExpressionException.java
@@ -1,20 +1,24 @@
 package evaluator;
 
 /**
- * A {@link IllegalPostFixExpressionException} is thrown at runtime when an invalid
- * PostFixExpression is encountered.
+ * A {@link IllegalPostFixExpressionException} is thrown at runtime when an
+ * invalid PostFixExpression is encountered.
+ * 
  * @author jcollard, jddevaug
  *
  */
-public class IllegalPostFixExpressionException extends RuntimeException {
-  
-  private static final long serialVersionUID = 1L;
+public class IllegalPostFixExpressionException extends RuntimeException
+{
 
-  public IllegalPostFixExpressionException() {
-    super();
-  }
+    private static final long serialVersionUID = 1L;
 
-  public IllegalPostFixExpressionException(String message) {
-    super(message);
-  }
+    public IllegalPostFixExpressionException()
+    {
+	super();
+    }
+
+    public IllegalPostFixExpressionException(String message)
+    {
+	super(message);
+    }
 }
