9d5f67855e943137e7cd49fd9dd0c294e3ca1296
johnnybegood1318 <johnnybegood1414@gmail.com>
2017-10-16 16:56:16 -0400

    add exceptions for equals and variables being used as operators

---
+++ support/language/EqualsException.java
@@ -0,0 +1,18 @@
+package language;
+
+public class EqualsException extends RuntimeException
+{
+
+    private static final long serialVersionUID = 1L;
+
+    public EqualsException()
+    {
+	super();
+    }
+
+    public EqualsException(String message)
+    {
+	super(message);
+    }
+
+}
---
+++ support/language/VariableException.java
@@ -0,0 +1,18 @@
+package language;
+
+public class VariableException extends RuntimeException
+{
+
+    private static final long serialVersionUID = 1L;
+
+    public VariableException()
+    {
+	super();
+    }
+
+    public VariableException(String message)
+    {
+	super(message);
+    }
+
+}
